7b62ccc4f50136521c2a33adf3b86df48cf6c5de
folorad <fduang@gmail.com>
2022-03-03 12:18:56 +0000

    Test for displaying 3x3 gris to the terminal

--- spec/TicTacToe_spec.rb
+++ spec/TicTacToe_spec.rb
@@ -26,60 +26,91 @@ describe TicTacToe do
 # 7 | 8 | 9   
 
 # ----Test 2: Build first row ----
-it " displays the first row of gameboard to the screen" do
+# it " displays the first row of gameboard to the screen" do
+#     #ARRANGE
+#     tictactoe = TicTacToe.new
+
+#     #RESULT
+#     gamedisplay = tictactoe.display_board
+
+#     #ASSERT
+#     #expecting the empty cells of the first row to be displayed to screen
+    
+#     # Output =>  |   |   
+
+#     expect(gamedisplay).to eq(["  " "|" "  " "|" "  "])
+
+#     end
+
+it 'can display 3x3 grid in the terminal' do
     #ARRANGE
     tictactoe = TicTacToe.new
 
     #RESULT
-    result = tictactoe.display_board
+    gamedisplay = tictactoe.display_board
+    board = tictactoe.board  #include the board method in this test as we use it in the 'display_board'
 
     #ASSERT
-    #expecting the empty cells of the first row to be displayed to screen
+    #expecting 3x3 grid to display in terminal 
+
+
+    # expect(gamedisplay).to eq(["  " "|" "  " "|" "  "])
+    # expect{my_function}.to output( 3 x 3 grid ).to_stdout
     
-    # Output =>  |   |   
+    expect{gamedisplay}.to output(" #{board[0]} " "|" " #{board[1]} " "|" " #{board[2]} \n"             "-----------\n" " #{board[3]} " "|" " #{board[4]} " "|" " #{board[5]} \n" "-----------\n" " #{board[6]} " "|" " #{board[7]} " "|" " #{board[8]} \n").to_stdout
 
-    expect(result).to eq(["  " "|" "  " "|" "  "])
-
+    
+    
     end
-
-    # ----Test 3: Build first row with index position numbers----
-    it " displays the first row of gameboard  with index numbers" do
-        #ARRANGE
-        tictactoe = TicTacToe.new
-    
-        #RESULT
-        result = tictactoe.display_board
-    
-        #ASSERT
-        #expecting  the empty cells of the first row & a separator to be displayed to screen
-
-         # Output => 
-         
-        #   |  |   
-
-        expect(result).to eq(["  |  |  "])
-    
-        end
-
-            # ----Test 4: Add separator to gameboard----
-    it " displays separator line under the the first row of gameboard" do
-        #ARRANGE
-        tictactoe = TicTacToe.new
-    
-        #RESULT
-        result = tictactoe.display_board
-    
-        #ASSERT
-        #expecting  the empty cells of the first row & a separator to be displayed to screen
-
-         # Output => 
-         
-        #   |  | 
-        # -----------   
-
-        expect(result).to eq([["  |  |  "]["-----------"]])
-    
-        end
-
 end
+
+
+
+
+
+
+
+
+#---- Discarded Tests ----
+
+
+#     # ----Test 3: Build first row ----
+# it " displays the first row of gameboard  with index numbers" do
+#         #ARRANGE
+#         tictactoe = TicTacToe.new
+    
+#         #RESULT
+#         result = tictactoe.display_board
+    
+#         #ASSERT
+#         #expecting  the empty cells of the first row & a separator to be displayed to screen
+
+#          # Output => 
+         
+#         #   |  |   
+
+#         expect(result).to eq(["  |  |  "])
+    
+#     end
+
+#             # ----Test 4: Add separator to gameboard----
+# it " displays separator line under the the first row of gameboard" do
+#         #ARRANGE
+#         tictactoe = TicTacToe.new
+    
+#         #RESULT
+#         result = tictactoe.display_board
+    
+#         #ASSERT
+#         #expecting  the empty cells of the first row & a separator to be displayed to screen
+
+#          # Output => 
+         
+#         #   |  | 
+#         # -----------   
+
+#         expect(result).to eq([["  |  |  "], ["-----------"]])
+    
+#         end
+
 
